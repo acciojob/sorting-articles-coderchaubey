5612f29e47b5b7207b69cc31636e02e5de5eabff
coderchaubey <115737868+coderchaubey@users.noreply.github.com>
2023-07-13 23:46:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+
+    const excludedWords = ['a', 'an', 'the'];
+
+    const sortedBands = bands.sort((a, b) => {
+      const nameA = removeExcludedWords(a);
+      const nameB = removeExcludedWords(b);
+      return nameA.localeCompare(nameB);
+    });
+
+    const bandList = document.getElementById('band');
+
+    sortedBands.forEach((band) => {
+      const listItem = document.createElement('li');
+      listItem.textContent = band;
+      bandList.appendChild(listItem);
+    });
+
+    function removeExcludedWords(string) {
+      return string.replace(/\b(a|an|the)\b/gi, '').trim();
+    }
